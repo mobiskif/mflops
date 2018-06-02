1b2652ea5188f2702816a28f6a61a62bb4f6c1fc
john <mobiskif@hotmail.com>
2018-06-02 09:06:26 +0300

    01.06.18

--- bi/src/main/java/ru/mobiskif/bi/MainActivity.java
+++ bi/src/main/java/ru/mobiskif/bi/MainActivity.java
@@ -3,14 +3,9 @@ package ru.mobiskif.bi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.anychart.anychart.AnyChart;
 import com.anychart.anychart.AnyChartView;
@@ -18,6 +13,7 @@ import com.anychart.anychart.DataEntry;
 import com.anychart.anychart.NameValueDataEntry;
 import com.anychart.anychart.Pie;
 import com.anychart.anychart.Venn;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.BufferedReader;
@@ -30,9 +26,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
     //GoogleSpeech tts;
-    ArrayList<String> fileContent = new ArrayList<>();
+    ArrayList fileContent = new ArrayList();
+    ArrayList datas = new ArrayList();
+    String headers = "A,B,C";
+    String baseName = "bi";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,60 +46,69 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (id == R.id.exit) {
             finish();
             return true;
-        }
-        else if (id == R.id.settings) {
+        } else if (id == R.id.settings) {
             //startActivityForResult(new Intent(this, SettingsActivity.class),0);
             return true;
-        }
-
-
-        else if (id == R.id.load) {
-            selectFile("*/*");
+        } else if (id == R.id.load) {
+            loadFromFile("*/*");
             return true;
-        }
-        else if (id == R.id.save) {
-            saveBase();
+        } else if (id == R.id.save) {
+            saveToBase();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void saveBase() {
+    private void saveToBase() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> user = new HashMap<>();
 
-        String[] heads = (fileContent.get(0)).split(",");
-        fileContent.remove(0);
-        for (String row: fileContent) {
-            String[] cols = row.split(",");
-            for (int i=0; i<cols.length; i++) {
+        String[] heads = headers.split(",");
+        for (Object row : fileContent) {
+            String[] cols = (row.toString()).split(",");
+            for (int i = 0; i < cols.length; i++) {
                 user.put(heads[i], cols[i]);
-                Log.d("jop",heads[i]+" = "+cols[i]);
+                Log.d("jop", heads[i] + " = " + cols[i]);
             }
-            db.collection("users").add(user);
-            Log.d("jop", "Отправлена строка: "+row);
+            db.collection(baseName).add(user);
+            Log.d("jop", "Отправлена строка: " + row);
         }
 
-        /*
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-        db.collection("users").add(user);
-        */
     }
 
-    private void selectFile(String type) {
+    void loadFromBase() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //db.collection("users")
+        db.collection(baseName)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (DocumentSnapshot document : task.getResult()) {
+                            Log.d("jop", String.valueOf(document.getData().toString()));
+                            datas.add(document.getData());
+                        }
+                    } else {
+                        Log.d("jop", "Error getting documents.", task.getException());
+                    }
+                })
+                //.addOnSuccessListener(documentSnapshots -> mRecyclerView.getAdapter().notifyDataSetChanged());
+                .addOnSuccessListener(documentSnapshots -> prepareCharts());
+
+    }
+
+    private void loadFromFile(String type) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType(type);
-        startActivityForResult(intent, 777);
+        startActivityForResult(intent, 1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            if (requestCode == 777 && data != null) {
+            if (requestCode == 1 && data != null) {
+                fileContent = new ArrayList<>();
 
                 try {
                     FileDescriptor fd = this
@@ -120,72 +128,58 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     } catch (IOException e) {
                         // log error
                     }
-                    //saveBase();
-                }
-                catch (IOException e) {
+                    headers = fileContent.get(0).toString();
+                    fileContent.remove(0);
+                    prepareCharts();
+                } catch (IOException e) {
                     //
                 }
             }
         }
     }
 
+    List getData(ArrayList<String> content) {
+        List<DataEntry> data = new ArrayList<>();
+        for (Object row : content) {
+            String[] cells = (row.toString()).split(",");
+            Log.d("jop", cells[1]);
+            data.add(new NameValueDataEntry(cells[0], cells[1], Integer.valueOf(cells[2])));
+        }
+        return data;
+    }
+
+    List getData2(ArrayList content) {
+        List<DataEntry> data = new ArrayList<>();
+        for (Object row : content) {
+            //String[] cells = (row.toString()).split(",");
+            Log.d("jop", row.toString());
+            Map<String, Object> user = (Map<String, Object>) row;
+            Log.d("jop", user.get("last").toString());
+
+            data.add(new NameValueDataEntry( user.get("first").toString(), user.get("last").toString(), Integer.valueOf(user.get("born").toString()) ));
+        }
+        return data;
+    }
+
+    void prepareCharts() {
+        AnyChartView anyChartView1 = findViewById(R.id.any_chart_view1);
+        AnyChartView anyChartView2 = findViewById(R.id.any_chart_view2);
+        Venn chart1 = AnyChart.venn();
+        Pie chart2 = AnyChart.pie();
+        //List<DataEntry> data = getData(fileContent);
+        List<DataEntry> data = getData2(datas);
+        chart1.setData(data);
+        chart2.setData(data);
+        anyChartView1.setChart(chart1);
+        anyChartView2.setChart(chart2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        RecyclerView mRecyclerView = findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-        mRecyclerView.setAdapter(new Adapter_FireBase());
-*/
-        //tts = new GoogleSpeech(this, null);
-
-
-
-        //Pie pie = AnyChart.pie();
-        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
-        AnyChartView anyChartView2 = findViewById(R.id.any_chart_view2);
-
-        Venn venn = AnyChart.venn();
-        Pie venn2 = AnyChart.pie();
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new NameValueDataEntry("A", "Data Science", 100));
-        data.add(new NameValueDataEntry("B", "Computer Science", 25));
-        data.add(new NameValueDataEntry("C", "Math and Statistics", 25));
-        data.add(new NameValueDataEntry("D", "Subject Matter Expertise", 25));
-        data.add(new NameValueDataEntry("A&B", "Computer Science", 50));
-        data.add(new NameValueDataEntry("A&C", "Math and Statistics", 50));
-        data.add(new NameValueDataEntry("A&D", "Subject Matter Expertise", 50));
-        data.add(new NameValueDataEntry("B&C", "Machine\\nLearning", 5));
-        data.add(new NameValueDataEntry("C&D", "Traditional\\nResearch", 5));
-        data.add(new NameValueDataEntry("D&B", "Traditional\\nSoftware", 5));
-        data.add(new NameValueDataEntry("B&C&D", "Unicorn", 5));
-
-        venn.setData(data);
-        venn2.setData(data);
-
-        venn.setStroke("#FFFFFF", 2d, null, null, null);
-
-        venn.getLabels().setFormat("{%Name}");
-
-        venn.getIntersections().getHovered().setFill("black", 0.25d);
-
-        venn.getIntersections().getLabels().setFontWeight("bold");
-        venn.getIntersections().getLabels().setFormat("{%Name}");
-
-        venn.getTooltip().setTitleFormat("{%Name}");
-
-        anyChartView.setChart(venn);
-        anyChartView2.setChart(venn2);
-
-
+        loadFromBase();
+        //prepareCharts();
     }
 
-    @Override
-    public void onClick(View v) {
-        //if (tts.isSpeaking()) tts.stop();
-        //tts.speak(((TextView) v).getText(), TextToSpeech.QUEUE_FLUSH, null, "1");
-    }
 }
